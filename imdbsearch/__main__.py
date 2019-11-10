5ddc130acd5aabda6551e97143d831ac82264d5e
aNightingale <anigh3@hotmail.com>
2019-11-11 06:31:01 +1100

    setup publish pypi (for testing)

--- imdbsearch/__main__.py
+++ imdbsearch/__main__.py
@@ -7,20 +7,6 @@ import json
 
 # GLOBAL VARIABLES
 
-
-"""
-Command-line interface Tool:
-	1. Queries IMDB
-	2. User enters movie star's name
-		a. If more than one actor has that name:
-			i. Return a menu for allowing selection of an individual based on available information
-	3. Prints the list of movies that the star has appeared in to screen
-		a. Default - Sorted in date order (oldest first)
-		b. Allow for reversal of order (newest first)
-	4. Allow option for sending result to a well-structured JSON document
-	5. Publish your new Python package to PyPi.org automatically from Github when a new tag is pushed 
-		a. Use a free CI/CD tool such as Travis/CircleCI"
-"""
 def main():
 
 	actordict = {}
